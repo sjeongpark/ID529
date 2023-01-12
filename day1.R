eeae84bcad19955317a6c079ccf4a3137c73f158
sjeongpark <lapinmd@outlook.kr>
2023-01-13 00:15:40 +0900

    project init

---
+++ day1.R
@@ -0,0 +1,33 @@
+x <- 1
+data.frame (
+  x = (a, b, c, d, e)
+  y = (20))  
+
+install.packages("tidyverse")
+
+help(data.frame)
+
+install.packages("devtools")
+
+git status
+
+install.packages("sas7bdat")
+library(sas7bdat)
+sas <- read.sas7bdat("HN20_ALL.sas7bdat")
+sasr
+
+install.packages('haven')
+library(haven)
+
+5 < 1
+
+x <- 1:10*10
+
+y <- seq(10, 100, by = 10)
+
+library(ggplot2)
+
+ggplot2::
+  
+install.packages("wesanderson")
+library(wesanderson)
